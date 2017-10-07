df03a4076a858cf627c5b848fdf474f3e7c5a362
Marco Cecconi <marco.cecconi@gmail.com>
2017-10-07 11:12:10 +0200

    initial import

---
+++ Italia.Sdk18App/CheckRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Italia.Sdk18App
+{
+    public class CheckRequest
+    {
+        public List<Check> Checks { get; set; }
+    }
+
+    public class Check
+    {
+        public string TipoOperazione { get; set; }
+        public string CodiceVoucher { get; set; }
+        public string PartitaIvaEsercente { get; set; }
+    }
+}
