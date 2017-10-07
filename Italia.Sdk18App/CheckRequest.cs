a60a1aac559269b21c598ab13e36951d83d688f6
Marco Cecconi <marco.cecconi@gmail.com>
2017-10-07 14:42:55 +0200

    Tutti i types

--- Italia.Sdk18App/CheckRequest.cs
+++ Italia.Sdk18App/CheckRequest.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 namespace Italia.Sdk18App
 {
-    public class CheckRequest
+    public class CheckRequestObj
     {
-        public List<Check> Checks { get; set; }
+        public Check CheckReq { get; set; }
     }
 
     public class Check
@@ -22,5 +22,32 @@ namespace Italia.Sdk18App
         public string Ambito { get; set; }
         public string Bene { get; set; }
         public double Importo { get; set; } // TODO: Cambiare a Decimal quando il WSDL e' corretto.
+    }
+
+    public class Confirm
+    {
+        public string TipoOperazione { get; set; }
+        public string CodiceVoucher { get; set; }
+        public double Importo { get; set; } // TODO: Cambiare a Decimal quando il WSDL e' corretto.
+    }
+
+    public class ConfirmResponse
+    {
+        public string Esito { get; set; }
+    }
+
+    public class CheckResponseObj
+    {
+        public CheckResponse CheckResp { get; set; }
+    }
+
+    public class ConfirmRequestObj
+    {
+        public Confirm CheckReq { get; set; }
+    }
+
+    public class ConfirmResponseObj
+    {
+        public ConfirmResponse CheckResp { get; set; }
     }
 }
